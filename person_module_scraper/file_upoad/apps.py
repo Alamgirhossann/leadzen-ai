1f79938c57dbdbd6a36550ecbadf1473adc3cddc
Raveen Beemsingh <raveen.b@gmail.com>
2021-07-20 18:34:45 +0530

    Pre Deploy Checkin for analystt/person_react-1626786285

--- person_module_scraper/file_upoad/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FileUpoadConfig(AppConfig):
-    name = 'file_upoad'
--- person_module_scraper/scrap/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import scrap
-from django.contrib.auth.decorators import login_required
-
-urlpatterns = [
-    path("<int:id>", login_required(scrap), name="scrap"),
-]
