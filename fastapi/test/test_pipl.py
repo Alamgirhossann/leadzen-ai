31eb758b2c4c1909523e431ae9addf01bc42ccd4
Raveen Beemsingh <raveen.b@gmail.com>
2021-08-26 03:21:54 +0530

    pipl search test added

--- fastapi/test/test_pipl.py
+++ fastapi/test/test_pipl.py
@@ -1,2 +1,24 @@
-def test_pipl_search():
-    assert True
+import httpx
+import pytest
+
+from app.main import app
+from app.pipl.router import PiplRequest, PiplName
+
+
+@pytest.mark.asyncio
+async def test_pipl_search():
+    async with httpx.AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.post(
+            "/api/pipl/search",
+            json=PiplRequest(
+                email="raveen.b@gmail.com", name=PiplName(last_name="Beemsingh")
+            ).dict(),
+        )
+
+        assert response
+        assert response.status_code == 200
+
+        data = response.json()
+        assert data
+
+        assert len(data) > 0
