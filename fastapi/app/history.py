1e97ac21e56ca6cf3e17216d25b43c880f35b506
Raveen Beemsingh <raveen.b@gmail.com>
2021-08-27 03:01:53 +0530

    database changes
    - added history - CR endpoints
    - refactored database to a common location, with single engine and database instance that is shared with all db users

---
+++ fastapi/app/history.py
@@ -0,0 +1,194 @@
+from datetime import datetime
+import json
+import uuid
+from typing import List, Optional, Dict
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from loguru import logger
+from pydantic import BaseModel, conlist
+
+from app.database import search_history
+from app.main import database
+from app.users import fastapi_users
+
+router = APIRouter(prefix="/history", tags=["History"])
+
+
+class SearchHistoryAddRequest(BaseModel):
+    search_id: str = str(uuid.uuid4())
+    search_type: str
+    search_term: str
+    search_results: conlist(item_type=Dict, min_items=1)
+
+
+class SearchHistoryAddResponse(BaseModel):
+    search_id: str
+
+
+class SearchHistoryGetMultipleRequest(BaseModel):
+    search_ids: conlist(item_type=str, min_items=1)
+
+
+class SearchHistoryShortResponse(BaseModel):
+    id: str
+
+    user_id: str
+    search_type: str
+    search_term: str
+
+    created_on: datetime
+
+
+class SearchHistoryFullResponse(BaseModel):
+    id: str
+
+    user_id: str
+    search_type: str
+    search_term: str
+    search_results: List[Dict]
+
+    created_on: datetime
+
+
+@router.get("/id/{search_id}", response_model=SearchHistoryFullResponse)
+async def get_search_history_by_id(
+    search_id: str, user=Depends(fastapi_users.get_current_active_user)
+):
+    logger.debug(f"{search_id=}, {user=}")
+    try:
+        query = (
+            f"SELECT * FROM search_history WHERE id = :search_id AND user_id = :user_id"
+        )
+
+        if not (
+            row := await database.fetch_one(
+                query=query, values={"search_id": search_id, "user_id": str(user.id)}
+            )
+        ):
+            logger.warning("Invalid Query Results")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Query Result"
+            )
+
+        logger.debug(f"{row=}")
+
+        processed_row = dict(row)
+        processed_row["search_results"] = json.loads(processed_row["search_results"])
+
+        return SearchHistoryFullResponse(**processed_row)
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        logger.critical(f"Exception Querying Database: {str(e)}")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Error Querying Database"
+        )
+
+
+@router.post("/ids", response_model=List[SearchHistoryFullResponse])
+async def get_search_history_by_multiple_ids(
+    request: SearchHistoryGetMultipleRequest,
+    user=Depends(fastapi_users.get_current_active_user),
+):
+    logger.debug(f"{request=}, {user=}")
+    try:
+        search_ids = ", ".join(["'" + x + "'" for x in request.search_ids])
+        query = (
+            f"SELECT * FROM search_history WHERE id IN ({search_ids}) AND "
+            "user_id = :user_id"
+        )
+
+        logger.debug(f"{query=}")
+        if not (
+            rows := await database.fetch_all(
+                query=query,
+                values={
+                    "user_id": str(user.id),
+                },
+            )
+        ):
+            logger.warning("Invalid Query Results")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Query Result"
+            )
+
+        logger.debug(f"{rows=}")
+
+        processed_rows = [dict(x) for x in rows]
+        processed_rows = [
+            x | {"search_results": json.loads(x["search_results"])}
+            for x in processed_rows
+        ]
+
+        return [SearchHistoryFullResponse(**x) for x in processed_rows if x]
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        logger.critical(f"Exception Querying Database: {str(e)}")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Error Querying Database"
+        )
+
+
+@router.get("/all", response_model=List[SearchHistoryShortResponse])
+async def get_all_search_history(user=Depends(fastapi_users.get_current_active_user)):
+    logger.debug(f"{user=}")
+    try:
+        query = "SELECT * FROM search_history WHERE user_id = :user_id ORDER BY id DESC"
+
+        if not (
+            rows := await database.fetch_all(
+                query=query, values={"user_id": str(user.id)}
+            )
+        ):
+            logger.warning("Invalid Query Results")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Query Result"
+            )
+
+        logger.debug(f"{rows=}")
+
+        processed_rows = [dict(x) for x in rows]
+
+        return [SearchHistoryShortResponse(**x) for x in processed_rows if x]
+
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        logger.critical(f"Exception Querying Database: {str(e)}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error Querying Database",
+        )
+
+
+@router.post("/add", response_model=SearchHistoryAddResponse)
+async def add_search_history(
+    request: SearchHistoryAddRequest,
+    user=Depends(fastapi_users.get_current_active_user),
+):
+    logger.debug(f"{request=}, {user=}")
+
+    try:
+        query = search_history.insert().values(
+            id=request.search_id,
+            user_id=str(user.id),
+            search_type=request.search_type,
+            search_term=request.search_term,
+            search_results=str(json.dumps(request.search_results)),
+            created_on=datetime.utcnow(),
+        )
+
+        logger.debug(f"{query=}")
+
+        row_id = await database.execute(query)
+
+        logger.debug(f"{row_id=}")
+
+        return SearchHistoryAddResponse(search_id=request.search_id)
+    except Exception as e:
+        logger.critical(f"Exception Inserting to Database: {str(e)}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error Inserting to Database",
+        )
--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -8,24 +8,30 @@ from loguru import logger
 from starlette import status
 
 from app.bulk.router import router as bulk_router
-from app.config import API_CONFIG_LINKEDIN_CSV_FILE, API_CONFIG_JWT_SECRET
+from app.config import (
+    API_CONFIG_LINKEDIN_CSV_FILE,
+    API_CONFIG_JWT_SECRET,
+)
 from app.customize_filter import router as filter_router
+from app.database import database
 from app.email import router as email_router
 from app.pipl.router import router as pipl_router
-from app.utils.proxy_curl import router as proxycurl_router
+from app.history import router as history_router
+from app.credits import router as credits_router
 from app.scraper import fetch_linkedin_cookie
-from app.search_result_operations import router as search_operations, database
-from app.utils.snov import router as snov_router
 from app.texau.router import router as texau_router
-from app.utils.truemail import router as truemail_router
 from app.users import fastapi_users
 from app.users import (
     jwt_authentication,
     on_after_register,
     on_after_forgot_password,
     after_verification_request,
-    database,
 )
+from app.utils.proxy_curl import router as proxycurl_router
+
+# from app.search_result_operations import router as search_operations
+from app.utils.snov import router as snov_router
+from app.utils.truemail import router as truemail_router
 
 current_active_user = fastapi_users.current_user(active=True)
 
@@ -53,14 +59,16 @@ app.include_router(router=pipl_router, prefix="/api")
 app.include_router(router=filter_router, prefix="/api")
 app.include_router(router=texau_router, prefix="/api")
 app.include_router(router=bulk_router, prefix="/api")
+app.include_router(router=history_router, prefix="/api")
+app.include_router(router=credits_router, prefix="/api")
 app.include_router(router=truemail_router, prefix="/api")
 app.include_router(router=snov_router, prefix="/api")
 app.include_router(router=proxycurl_router, prefix="/api")
-app.include_router(
-    router=search_operations,
-    prefix="/api",
-    dependencies=[Depends(fastapi_users.get_current_active_user)],
-)
+# app.include_router(
+#     router=search_operations,
+#     prefix="/api",
+#     dependencies=[Depends(fastapi_users.get_current_active_user)],
+# )
 
 app.include_router(
     fastapi_users.get_auth_router(jwt_authentication),
@@ -90,11 +98,6 @@ app.include_router(
     fastapi_users.get_users_router(), prefix="/api/users", tags=["Users"]
 )
 app.include_router(router=email_router, prefix="/api")
-app.include_router(
-    router=search_operations,
-    prefix="/api",
-    dependencies=[Depends(fastapi_users.get_current_active_user)],
-)
 
 
 # @app.on_event("startup")
@@ -127,12 +130,13 @@ def refresh_linkedin_cookie_manually():
 
 @app.on_event("startup")
 async def startup():
-    logger.debug("In database Connectin....")
+    logger.info("Connecting to Database")
     await database.connect()
 
 
 @app.on_event("shutdown")
 async def shutdown():
+    logger.info("Disconnecting from Database")
     await database.disconnect()
 
 
