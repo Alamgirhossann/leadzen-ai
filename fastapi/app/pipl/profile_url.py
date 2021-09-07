6c947120cee942c292a2b2c3652d8ea4ca84af33
ShivPatel32549 <87748960+ShivPatel32549@users.noreply.github.com>
2021-09-07 13:12:16 +0545

    Designed excel file which are send to user on export button

--- fastapi/app/pipl/profile_url.py
+++ fastapi/app/pipl/profile_url.py
@@ -7,7 +7,9 @@ from pydantic import BaseModel
 from app.config import API_CONFIG_PIPL_BASE_URL, API_CONFIG_PIPL_API_KEY
 from app.pipl.common import write_to_file, search_all
 
-
+import openpyxl
+from openpyxl import load_workbook
+from openpyxl.styles import PatternFill
 class PiplDetailsFromProfileUrlRequest(BaseModel):
     profile_urls: List[str]
     filename: Optional[str] = None
@@ -15,6 +17,11 @@ class PiplDetailsFromProfileUrlRequest(BaseModel):
 
 class PiplDetailsFromProfileUrlResponse(BaseModel):
     filename: str
+
+class RequestForExcelData(BaseModel):
+    filename:str
+
+
 
 
 async def execute_task(request: PiplDetailsFromProfileUrlRequest):
@@ -30,9 +37,39 @@ async def execute_task(request: PiplDetailsFromProfileUrlRequest):
     if not (responses := await search_all(urls=urls, slugs=profile_urls)):
         logger.error("Error Getting Data")
         return
-
     if not any(responses):
         logger.error("no valid responses found")
         return
 
     await write_to_file(responses=responses, filename=request.filename)
+
+async def excel_design(request:RequestForExcelData):
+    filename=request.filename
+    wb = load_workbook(f'{filename}')
+    wb.create_sheet('Index', 0)
+    index_sheet = wb.get_sheet_by_name('Index')
+    index_sheet.merge_cells('A1:W36')
+    img1 = openpyxl.drawing.image.Image(f'./app/Excel/Index.png')
+    img1.anchor = 'A1'
+    index_sheet.add_image(img1)
+    wb.create_sheet('Disclaimer', 2)
+    dis_sheet = wb.get_sheet_by_name('Disclaimer')
+    dis_sheet.merge_cells('A1:X31')
+    img2 = openpyxl.drawing.image.Image(f'./app/Excel/Disclaimer.png')
+    img2.anchor = 'A1'
+    dis_sheet.add_image(img2)
+    financial_advisor = wb.get_sheet_by_name('Sheet1')
+    financial_advisor.title = "Financial Advisor"
+    for rows in financial_advisor.iter_rows(min_row=1, max_row=1, min_col=1):
+        for cell in rows:
+            cell.fill = PatternFill(fgColor="B4C9D9", patternType="solid")
+    # number_of_rows = financial_advisor.max_row
+    number_of_col = financial_advisor.max_column
+    last_column = chr(65 + number_of_col - 1)
+    lastcell = last_column + str(number_of_col)
+    financial_advisor.move_range(f"A1:{lastcell}", rows=8, cols=0)
+    financial_advisor.merge_cells('A1:W8')
+    img3 = openpyxl.drawing.image.Image(f'./app/Excel/FA_img.png')
+    img3.anchor = 'A1'
+    financial_advisor.add_image(img3)
+    wb.save(filename)
