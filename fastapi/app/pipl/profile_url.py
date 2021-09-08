20776178e7b49fcbb428d63beb2f68620afda796
ShivPatel32549 <87748960+ShivPatel32549@users.noreply.github.com>
2021-09-08 12:11:25 +0545

    worked on raveen sir's comments

--- fastapi/app/pipl/profile_url.py
+++ fastapi/app/pipl/profile_url.py
@@ -39,39 +39,51 @@ async def execute_task(request: PiplDetailsFromProfileUrlRequest):
 
 def add_excel_template_to_file(outgoing_filename):
     if  (outgoing_filename==""):
-        logger.error("No excel file found")
-        return
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=str("Excel file not found"),
+        )
     else:
         wb = load_workbook(f'{outgoing_filename}')
         if (wb==""):
-            logger.error("Error in loading work book of excel file")
-            return
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=str("Error in Work book Loading"),
+            )
         else:
-            wb.create_sheet('Index', 0)
-            index_sheet = wb.get_sheet_by_name('Index')
-            index_sheet.merge_cells('A1:W36')
-            img1 = openpyxl.drawing.image.Image(f'./app/Excel/Index.png')
-            img1.anchor = 'A1'
-            index_sheet.add_image(img1)
-            wb.create_sheet('Disclaimer', 2)
-            dis_sheet = wb.get_sheet_by_name('Disclaimer')
-            dis_sheet.merge_cells('A1:X31')
-            img2 = openpyxl.drawing.image.Image(f'./app/Excel/Disclaimer.png')
-            img2.anchor = 'A1'
-            dis_sheet.add_image(img2)
-            financial_advisor = wb.get_sheet_by_name('Sheet1')
-            financial_advisor.title = "Financial Advisor"
-            for rows in financial_advisor.iter_rows(min_row=1, max_row=1, min_col=1):
-                for cell in rows:
-                    cell.fill = PatternFill(fgColor="B4C9D9", patternType="solid")
-            # number_of_rows = financial_advisor.max_row
-            number_of_col = financial_advisor.max_column
-            last_column = chr(65 + number_of_col - 1)
-            lastcell = last_column + str(number_of_col)
-            financial_advisor.move_range(f"A1:{lastcell}", rows=8, cols=0)
-            financial_advisor.merge_cells('A1:W8')
-            img3 = openpyxl.drawing.image.Image(f'./app/Excel/FA_img.png')
-            img3.anchor = 'A1'
-            financial_advisor.add_image(img3)
-            wb.save(outgoing_filename)
-            return outgoing_filename
+            try:
+                wb.create_sheet('Index', 0)
+                index_sheet = wb.get_sheet_by_name('Index')
+                index_sheet.merge_cells('A1:W36')
+                img1 = openpyxl.drawing.image.Image(f'./app/Excel/Index.png')
+                img1.anchor = 'A1'
+                index_sheet.add_image(img1)
+                wb.create_sheet('Disclaimer', 2)
+                dis_sheet = wb.get_sheet_by_name('Disclaimer')
+                dis_sheet.merge_cells('A1:X31')
+                img2 = openpyxl.drawing.image.Image(f'./app/Excel/Disclaimer.png')
+                img2.anchor = 'A1'
+                dis_sheet.add_image(img2)
+                financial_advisor = wb.get_sheet_by_name('Sheet1')
+                financial_advisor.title = "Financial Advisor"
+                for rows in financial_advisor.iter_rows(min_row=1, max_row=1, min_col=1):
+                    for cell in rows:
+                        cell.fill = PatternFill(fgColor="B4C9D9", patternType="solid")
+                # number_of_rows = financial_advisor.max_row
+                number_of_col = financial_advisor.max_column
+                last_column = chr(65 + number_of_col - 1)
+                lastcell = last_column + str(number_of_col)
+                financial_advisor.move_range(f"A1:{lastcell}", rows=8, cols=0)
+                financial_advisor.merge_cells('A1:W8')
+                img3 = openpyxl.drawing.image.Image(f'./app/Excel/FA_img.png')
+                img3.anchor = 'A1'
+                financial_advisor.add_image(img3)
+                wb.save(outgoing_filename)
+                return outgoing_filename
+            except Exception as e:
+                logger.critical("Error>>>" + str(e))
+                raise HTTPException(
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail="Error in add excel template to file",
+                )
+
