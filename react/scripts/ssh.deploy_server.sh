f48c5e3b8b69521398531c477a980e7a9a5d858a
Raveen Beemsingh <raveen.b@gmail.com>
2021-09-13 18:32:54 +0530

    Pre Deploy Checkin for analystt/person_react-1631538174

--- react/scripts/ssh.deploy_server.sh
+++ react/scripts/ssh.deploy_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REMOTE_SERVER="ubuntu@3.109.59.177"
-PEM_FILE="/Users/analystt/dev/repos/people-module/react/linux2-pinaki.pem"
+PEM_FILE="/Users/analystt/dev/analystt/repos/people-module/linux2-pinaki.pem"
 
 ssh -i "$PEM_FILE" "$REMOTE_SERVER"
 
