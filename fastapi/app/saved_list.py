5e6091136a31e390e91cd70854a6dca0ed7ce77f
Kishan Jaiswal <kishan.jaiswal@kapso.in>
2021-09-16 10:40:14 +0545

    end point to get names is created

--- fastapi/app/saved_list.py
+++ fastapi/app/saved_list.py
@@ -14,6 +14,7 @@ router = APIRouter(prefix="/save_list", tags=["Search SaveList"])
 
 
 class SaveListRequest(BaseModel):
+    save_list_name:str
     save_list_results: Dict
     search_type: str
 
@@ -36,6 +37,7 @@ async def add_search_save_list(
     try:
         query = search_saved.insert().values(
             id=id,
+            save_list_name=request.save_list_name,
             user_id=str(user.id),
             search_type=request.search_type,
             save_list_results=json.dumps(request.save_list_results),
@@ -109,12 +111,48 @@ async def get_all_save_list(user=Depends(fastapi_users.get_current_active_user))
 
         logger.debug(f"{rows=}")
         processed_rows = [dict(x) for x in rows]
-
         for x in processed_rows:
             x["save_list_results"] = json.loads(x['save_list_results'])
-
         # return SaveListFullResponse(save_list_results=processed_rows)
         return processed_rows
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        logger.critical(f"Exception Querying Database: {str(e)}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error Querying Database",
+        )
+
+
+@router.get("/all_names")
+async def get_all_save_list(user=Depends(fastapi_users.get_current_active_user)):
+    logger.debug(f"{user=}")
+    try:
+        query = "SELECT save_list_name FROM search_saved WHERE user_id = :user_id ORDER BY created_on DESC"
+
+        if not (
+                rows := await database.fetch_all(
+                    query=query, values={"user_id": str(user.id)}
+                )
+        ):
+            print("rows>>>>", rows)
+            logger.warning("Invalid Query Results")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Query Result"
+            )
+        save_list_names=[]
+        for i in rows:
+            check=True
+            for j in save_list_names:
+                if i[0]==j['save_list_name']:
+                    check=False
+                    break;
+            if check:
+                save_list_names.append({'save_list_name': i[0]})
+
+        return save_list_names
+
     except HTTPException as e:
         raise e
     except Exception as e:
