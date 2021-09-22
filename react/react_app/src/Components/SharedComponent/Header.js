d47cdec02ef9565fc9844d281204de5df0a2c98d
MD ALAMGIR HOSSAN <alamgir.hossan@kapso.in>
2021-09-22 16:52:54 +0600

    leadzen linked to leadzen ai

--- react/react_app/src/Components/SharedComponent/Header.js
+++ react/react_app/src/Components/SharedComponent/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
       <header className="header-area">
         <nav className="header-navbar navbar navbar-expand-xl bg-light">
           <div className="container-fluid" style={{ paddingRight: "0px" }}>
-            <a className="navbar-brand" href="/repeatedUser">
+            <a className="navbar-brand" href="https://leadzen.ai/">
               <img src="assets/images/logo- without BG.png" alt="title" />
             </a>
 
