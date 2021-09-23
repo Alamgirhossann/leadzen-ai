62ada976b3b4d1ac9e2f72a6d4064eccdb61252e
ShivPatel32549 <87748960+ShivPatel32549@users.noreply.github.com>
2021-09-23 14:15:53 +0545

    Excel template is designed as per the requirements

--- fastapi/app/pipl/profile_url.py
+++ fastapi/app/pipl/profile_url.py
@@ -8,7 +8,7 @@ from pydantic import BaseModel
 
 from app.config import API_CONFIG_PIPL_BASE_URL, API_CONFIG_PIPL_API_KEY, API_CONFIG_EXCEL_FILE_PATH
 from app.pipl.common import write_to_file, search_all
-
+from datetime import date
 import openpyxl
 from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
@@ -47,48 +47,42 @@ async def execute_task(request: PiplDetailsFromProfileUrlRequest):
     await write_to_file(responses=responses, filename=request.filename)
 
 
-def add_excel_template_to_file(outgoing_filename):
-    if outgoing_filename == "":
+def add_excel_template_to_file(outgoing_filename, user):
+    if  (outgoing_filename==""):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=str("Excel file not found"),
         )
     else:
-        wb = load_workbook(f'{outgoing_filename}')
-        if wb == "":
+        wb_orginal = load_workbook(f'{outgoing_filename}')
+        if (wb_orginal==""):
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
                 detail=str("Error in Work book Loading"),
             )
         else:
             try:
-                wb.create_sheet('Index', 0)
-                index_sheet = wb.get_sheet_by_name('Index')
-                index_sheet.merge_cells('A1:W36')
-                img1 = openpyxl.drawing.image.Image(f'{API_CONFIG_EXCEL_FILE_PATH}/Index.png')
-                img1.anchor = 'A1'
-                index_sheet.add_image(img1)
-                wb.create_sheet('Disclaimer', 2)
-                dis_sheet = wb.get_sheet_by_name('Disclaimer')
-                dis_sheet.merge_cells('A1:X31')
-                img2 = openpyxl.drawing.image.Image(f'{API_CONFIG_EXCEL_FILE_PATH}/Disclaimer.png')
-                img2.anchor = 'A1'
-                dis_sheet.add_image(img2)
-                financial_advisor = wb.get_sheet_by_name('Sheet1')
-                financial_advisor.title = "Financial Advisor"
-                for rows in financial_advisor.iter_rows(min_row=1, max_row=1, min_col=1):
+                wb_templet=load_workbook(f'{API_CONFIG_EXCEL_FILE_PATH}/LeadZen_template.xlsx')
+                index_sheet = wb_templet.get_sheet_by_name('Index')
+                data = wb_orginal.get_sheet_by_name('Sheet1')
+                financial_advisor = wb_templet.get_sheet_by_name('Financial Advisor')
+                number_of_rows = data.max_row
+                number_of_col = data.max_column
+                for i in range(1, number_of_rows + 1):
+                    for j in range(1, number_of_col + 1):
+                        c = data.cell(row=i, column=j)
+                        financial_advisor.cell(row=i+5, column=j).value = c.value
+                for rows in financial_advisor.iter_rows(min_row=6, max_row=6, min_col=1):
                     for cell in rows:
                         cell.fill = PatternFill(fgColor="B4C9D9", patternType="solid")
-                # number_of_rows = financial_advisor.max_row
-                number_of_col = financial_advisor.max_column
-                last_column = chr(65 + number_of_col - 1)
-                lastcell = last_column + str(number_of_col)
-                financial_advisor.move_range(f"A1:{lastcell}", rows=8, cols=0)
-                financial_advisor.merge_cells('A1:W8')
-                img3 = openpyxl.drawing.image.Image(f'{API_CONFIG_EXCEL_FILE_PATH}/FA_img.png')
-                img3.anchor = 'A1'
-                financial_advisor.add_image(img3)
-                wb.save(outgoing_filename)
+                no_of_record=index_sheet['J18']
+                requested_by = index_sheet['J20']
+                request_date = index_sheet['J22']
+                no_of_record.value=number_of_rows-1
+                requested_by.value=user.username
+                today = date.today()
+                request_date.value=today.strftime("%d/%m/%Y")
+                wb_templet.save(outgoing_filename)
                 return outgoing_filename
             except Exception as e:
                 logger.critical("Error>>>" + str(e))
