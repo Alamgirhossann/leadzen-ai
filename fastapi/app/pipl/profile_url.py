be74e5db02367d777134200e1e7d5453d7b4d893
ShivPatel32549 <87748960+ShivPatel32549@users.noreply.github.com>
2021-09-24 16:40:22 +0545

    search bar and search functionality is added at top of search result page of both company and people search result page

--- fastapi/app/pipl/profile_url.py
+++ fastapi/app/pipl/profile_url.py
@@ -62,27 +62,27 @@ def add_excel_template_to_file(outgoing_filename, user):
             )
         else:
             try:
-                wb_templet=load_workbook(f'{API_CONFIG_EXCEL_FILE_PATH}/LeadZen_template.xlsx')
-                index_sheet = wb_templet.get_sheet_by_name('Index')
+                wb_template=load_workbook(f'{API_CONFIG_EXCEL_FILE_PATH}/LeadZen_template.xlsx')
+                index_sheet = wb_template.get_sheet_by_name('Index')
                 data = wb_orginal.get_sheet_by_name('Sheet1')
-                financial_advisor = wb_templet.get_sheet_by_name('Financial Advisor')
-                number_of_rows = data.max_row
-                number_of_col = data.max_column
-                for i in range(1, number_of_rows + 1):
-                    for j in range(1, number_of_col + 1):
-                        c = data.cell(row=i, column=j)
-                        financial_advisor.cell(row=i+5, column=j).value = c.value
-                for rows in financial_advisor.iter_rows(min_row=6, max_row=6, min_col=1):
+                financial_advisor_sheet = wb_template.get_sheet_by_name('Financial Advisor')
+                data_sheet_row_count = data.max_row
+                data_sheet_col_count = data.max_column
+                for row in range(1, data_sheet_row_count + 1):
+                    for col in range(1, data_sheet_col_count + 1):
+                        c = data.cell(row=row, column=col)
+                        financial_advisor_sheet.cell(row=row+5, column=col).value = c.value
+                for rows in financial_advisor_sheet.iter_rows(min_row=6, max_row=6, min_col=1):
                     for cell in rows:
                         cell.fill = PatternFill(fgColor="B4C9D9", patternType="solid")
                 no_of_record=index_sheet['J18']
                 requested_by = index_sheet['J20']
                 request_date = index_sheet['J22']
-                no_of_record.value=number_of_rows-1
+                no_of_record.value=data_sheet_row_count-1
                 requested_by.value=user.username
                 today = date.today()
                 request_date.value=today.strftime("%d/%m/%Y")
-                wb_templet.save(outgoing_filename)
+                wb_template.save(outgoing_filename)
                 return outgoing_filename
             except Exception as e:
                 logger.critical("Error>>>" + str(e))
