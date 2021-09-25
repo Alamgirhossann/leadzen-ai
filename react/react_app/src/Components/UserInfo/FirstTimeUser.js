9a2ef85f055668e0167f8cec4f7ce859497f328e
MD ALAMGIR HOSSAN <alamgir.hossan@kapso.in>
2021-09-25 18:09:22 +0600

    Update FirstTimeUser.js

--- react/react_app/src/Components/UserInfo/FirstTimeUser.js
+++ react/react_app/src/Components/UserInfo/FirstTimeUser.js
@@ -136,11 +136,6 @@ const FirstTimeUser = () => {
                 <h6 className="text-danger mb-3">Customize your search</h6>
                 <Filters />
               </div>
-              {/* <SpecificSearchBtn/>
-              <div className="sidebar-search-for sidebar-widget pt-4 my-3">
-                <h6 className="text-danger mb-3">Customize your search</h6>
-                <Filters />
-              </div> */}
               <BulkSearch />
               <SidebarExtractContact />
             </div>
