12a50c9c7a2c2ebcdb91d23ebf1faa2e9d3bfb14
Kishan Jaiswal <kishan.jaiswal@kapso.in>
2021-09-29 14:05:57 +0545

    not done yet getting error

---
+++ fastapi/test/test_sendinblue.py
@@ -0,0 +1,25 @@
+import asyncio
+
+import httpx
+import pytest
+from app.main import app
+from app.bulk.router import BulkExportToExcelRequest
+from test.common import TEST_CONFIG_HEADERS
+
+
+@pytest.mark.asyncio
+async def test_sendinblue_export_excel_sucess_mail():
+    async with httpx.AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.post(
+            "/api/bulk_upload/export/excel",
+            json=BulkExportToExcelRequest(
+                profile_urls=["https://www.linkedin.com/in/sauravbarun"],
+                hash_key_list=[{"https://www.linkedin.com/in/sauravbarun": "1f798b31709c734b06bb2dc29941470de8e9862f9a292c7be72f6e062b5afd67"}],
+            ).dict(),
+            headers=TEST_CONFIG_HEADERS,
+        )
+    assert response
+    assert response.status_code == 200
+
+
+
