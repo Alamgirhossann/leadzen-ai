cae91cc84fc411a55ccb49d8d6623556d336b0da
Raveen Beemsingh <raveen.b@gmail.com>
2021-09-30 13:45:49 +0530

    getting things ready for test

--- scripts/run.local.react.sh
+++ scripts/run.local.react.sh
@@ -1,23 +1,101 @@
 # !/bin/bash
-echo "-----------------------------"
-echo "Building Docker Images"
+echo "> ----------------------"
+echo "> Building Docker Images"
+echo "> ----------------------"
 
+echo "> -----------------------------"
+echo "> Building FastAPI Docker Image"
+echo "> -----------------------------"
 cd fastapi
 sudo ./scripts/build.docker.sh
+if [ $? != 0 ]; then
+    echo ">>> Unable to build FastAPI Docker Image"
+    cd ..
+    exit 1
+fi
+echo "> Done"
+echo "..."
 
+
+echo "> ---------------------------"
+echo "> Building React Docker Image"
+echo "> ---------------------------"
 cd ../react
 sudo ./scripts/build.docker.sh
+if [ $? != 0 ]; then
+    echo ">>> Unable to build React Docker Image"
+    cd ..
+    exit 1
+fi
+echo "> Done"
+echo "..."
 
-cd ../api
+echo "> ----------------------------------------------------"
+echo "> Copying FastAPI database definitions to External API"
+echo "> ----------------------------------------------------"
+cd ..
+cp fastapi/app/database.py api/app/database.py
+if [ $? != 0 ]; then
+    echo ">>> Error Copying FastAPI database definitions"
+    exit 1
+fi
+echo "> Done"
+echo "..."
+
+
+echo "> ----------------------------------"
+echo "> Building External API Docker Image"
+echo "> ----------------------------------"
+cd api
 sudo ./scripts/build.docker.sh
-
-echo "-----------------------------"
+if [ $? != 0 ]; then
+    echo ">>> Unable to build External API Docker Image"
+    cd ..
+    exit 1
+fi
+echo "> Done"
+echo "..."
 
 cd ..
 
-echo "Finished Building Docker Images"
-echo "------------------------------"
+echo "> -------------------------------"
+echo "> Finished Building Docker Images"
+echo "> -------------------------------"
 
-echo "Running Docker Container"
-echo "-------------------------------"
+echo "> -----------------------------------"
+echo "> Creating some directories if needed"
+echo "> -----------------------------------"
+if [[ ! -d shared/database/sqlite ]]; 
+then
+    echo "> Creating SQLite directories"
+    mkdir -p shared/database/sqlite
+fi
+if [[ ! -d shared/database/postgres ]]; 
+then
+    echo "> Creating Postgres directories"
+    mkdir -p shared/database/postgres
+fi
+if [[ ! -d shared/database/pgadmin ]]; 
+then
+    echo "> Creating pgAdmin directories"
+    mkdir -p shared/database/pgadmin
+fi
+echo "> Done"
+echo "..."
+
+echo "> --------------------------------"
+echo "> Stopping Other Docker Containers"
+echo "> --------------------------------"
+sudo docker rm -f person_fastapi_external person_fastapi person_react selenium redis pgadmin postgres
+if [ $? != 0 ]; then
+    echo ">>> Unable to Stop Docker Containers"
+    exit 1
+fi
+echo "> Done"
+echo "..."
+
+
+echo "> -------------------------"
+echo "> Running Docker Containers"
+echo "> -------------------------"
 sudo docker-compose -f docker-compose.dev.yml up --remove-orphans
