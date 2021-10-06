4427e1d3367e3e77795ea65c2ee9447b21317c45
Raveen Beemsingh <raveen.b@gmail.com>
2021-10-07 01:52:43 +0530

    mofications to handle the phone verification properly

--- fastapi/app/utils/phone_verification.py
+++ fastapi/app/utils/phone_verification.py
@@ -1,58 +1,123 @@
 import asyncio
+from typing import List, Dict, Optional, Text
+
 import httpx
-import json
-import requests
-from typing import List, Optional
-from app.config import (API_CONFIG_GET_PHONE_VERIFICATION_URL,
-                        API_CONFIG_GET_PHONE_VERIFICATION_APIKEY,
-                        API_CONFIG_GET_PHONE_VERIFICATION_CALLBACK_URL)
-from fastapi import APIRouter,HTTPException,Depends
-from pydantic.main import BaseModel
-from typing import Optional
+from fastapi import APIRouter, HTTPException, status
 from loguru import logger
-from starlette import status
-from app.users import fastapi_users, get_user
+from pydantic import BaseModel, conlist
+import phonenumbers
+
+from app.config import (
+    API_CONFIG_GET_PHONE_VERIFICATION_URL,
+    API_CONFIG_GET_PHONE_VERIFICATION_APIKEY,
+    API_CONFIG_GET_PHONE_VERIFICATION_CALLBACK_URL,
+    API_CONFIG_HTTP_CALL_TIMEOUT_IN_SECONDS,
+)
 
 router = APIRouter(prefix="/phone", tags=["Phone Verification"])
 
 
 class PhoneRequest(BaseModel):
-    phones: List[str]=[]
+    phones: conlist(item_type=str, min_items=1)
 
-@router.post("/phone_verification")
-async def phone_verification(request:PhoneRequest):
-    # logger.info(f"{user=}")
-    if request.phones:
-        async def async_call(phone_number):
-            url = API_CONFIG_GET_PHONE_VERIFICATION_URL
-            payload = {
-                "apikey": API_CONFIG_GET_PHONE_VERIFICATION_APIKEY,
-                "phoneno": phone_number,
-                "callbackurl": API_CONFIG_GET_PHONE_VERIFICATION_CALLBACK_URL
-            }
-            async with httpx.AsyncClient() as client:
-                timeout = httpx.Timeout(20.0)
-                response = await client.get(url, params=payload,timeout=timeout)
-                if response.status_code == 200:
-                    data = json.loads(response.text)
-                    data["phone"] = phone_number
-                    logger.debug(data)
-                    return data
-                elif response.status_code == 400:
-                    raise HTTPException(
-                        status_code=status.HTTP_400_BAD_REQUEST,
-                        detail=str("Phone Verification: Bad request"),
-                    )
+
+@router.post("/phone_verification", response_model=List[Dict])
+async def phone_verification(request: PhoneRequest):
+    logger.info(f"{request=}")
+
+    def clean_phone_numbers(phones: List[Text]) -> Optional[List[Dict]]:
+        def fallback(phone: Text) -> Optional[Text]:
+            try:
+                x = str(int(phone))
+                if len(x) != 10:
+                    logger.warning("Not 10 Digit Phone Number")
+                    return None
                 else:
-                    raise HTTPException(
-                        status_code=status.HTTP_404_NOT_FOUND,
-                        detail=str("Phone Verification: Data not found"),
-                    )
-        coros = [async_call(_i) for _i in request.phones]
-        results = await asyncio.gather(*coros)
-        return results
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=str("Phone: Invalid Phone Number"),
+                    logger.success(x)
+                    return x
+            except Exception as e:
+                logger.warning(e)
+                return None
+
+        def parse(phone: str) -> Optional[Dict]:
+            try:
+                return {
+                    "cleaned": str(phonenumbers.parse(phone, None).national_number),
+                    "raw": phone,
+                }
+            except Exception as e:
+                logger.warning(f"Exception Parsing Phone Number {phone=}: {e}")
+
+                logger.warning(f"Using Fallback Parsing Phone Number {phone=}")
+                if not (cleaned := fallback(phone)):
+                    return None
+
+                return {"cleaned": cleaned, "raw": phone}
+
+        phones = [parse(x) for x in phones if x]
+
+        if not any(phones):
+            logger.warning("No Results")
+            return None
+
+        phones = [x for x in phones if x]
+
+        logger.success(phones)
+
+        return phones
+
+    async def verify_one(clean_phone: Text, raw_phone: Text, client: httpx.AsyncClient):
+        response = await client.get(
+            API_CONFIG_GET_PHONE_VERIFICATION_URL,
+            params={
+                "apikey": API_CONFIG_GET_PHONE_VERIFICATION_APIKEY,
+                "phoneno": clean_phone,
+                "callbackurl": API_CONFIG_GET_PHONE_VERIFICATION_CALLBACK_URL,
+            },
+            timeout=httpx.Timeout(API_CONFIG_HTTP_CALL_TIMEOUT_IN_SECONDS),
         )
+
+        if not response:
+            logger.error("Invalid Response")
+            return None
+
+        if response.status_code != 200:
+            logger.error(f"Invalid Status Response: {response.status_code=}")
+            return None
+
+        if not (data := response.json()):
+            logger.error("Invalid Response Data:")
+            return None
+
+        data["phone"] = clean_phone
+        data["raw_phone"] = raw_phone
+        logger.debug(f"{data=}")
+
+        return data
+
+    if not (
+        cleaned_and_de_duplicated_phone_numbers := clean_phone_numbers(
+            phones=list(set(request.phones))
+        )
+    ):
+        logger.warning("No Valid Phone Number Found in Request")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="No Valid Phone Numbers"
+        )
+
+    async with httpx.AsyncClient() as client:
+        coroutines = [
+            verify_one(clean_phone=x["cleaned"], raw_phone=x["raw"], client=client)
+            for x in cleaned_and_de_duplicated_phone_numbers
+        ]
+        results = await asyncio.gather(*coroutines)
+
+    if not any(results):
+        logger.warning("No Verification Results")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="No Results Found"
+        )
+
+    logger.debug(f"{results=}")
+
+    return results
