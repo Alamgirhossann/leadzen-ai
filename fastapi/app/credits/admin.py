f94af21292498dff1f685cf4a46065211756eb31
Pinaki Dasgupta <pinaki@kapso.in>
2021-10-08 14:08:34 +0000

    added email credit deduction

--- fastapi/app/credits/admin.py
+++ fastapi/app/credits/admin.py
@@ -12,24 +12,24 @@ router = APIRouter(prefix="/credits/admin", tags=["Credit Admin Operations"])
 
 @router.put("/add", response_model=UserCreditResponse)
 async def add_all_credits_to_users(
-    request: UserCreditRequest,
-    background_tasks: BackgroundTasks,
-    user=Depends(fastapi_users.get_current_superuser),
+        request: UserCreditRequest,
+        background_tasks: BackgroundTasks,
+        user=Depends(fastapi_users.get_current_superuser),
 ):
     logger.debug(f"{user=},>>>{request=}")
     try:
 
         update_query = (
             users.update()
-            .returning(users.c.id)
-            .values(
+                .returning(users.c.id)
+                .values(
                 profile_credit=users.c.profile_credit + request.profile_credit,
                 email_credit=users.c.email_credit + request.email_credit,
                 total_profile_credits=users.c.total_profile_credits
-                + request.profile_credit,
+                                      + request.profile_credit,
                 total_email_credits=users.c.total_email_credits + request.email_credit,
             )
-            .where(users.c.email == request.email)
+                .where(users.c.email == request.email)
         )
         logger.debug(f"{update_query=}")
 
@@ -42,7 +42,7 @@ async def add_all_credits_to_users(
         query = "SELECT * FROM public.user WHERE id = :user_id"
 
         if not (
-            rows := await database.fetch_all(query=query, values={"user_id": str(row)})
+                rows := await database.fetch_all(query=query, values={"user_id": str(row)})
         ):
             logger.warning("Invalid Query Results")
             raise HTTPException(
@@ -69,11 +69,12 @@ async def add_all_credits_to_users(
 
 @router.put("/deduct/{credit_type}", response_model=UserCreditResponse)
 async def deduct_credit_by_type(
-    credit_type: str,
-    user=Depends(fastapi_users.get_current_active_user),
+        credit_type: str,
+        user=Depends(fastapi_users.get_current_active_user),
 ):
     logger.debug(f"{user=}")
     credit_res = await deduct_credit(credit_type, user)
+    logger.debug(f"{credit_type=}")
     return UserCreditResponse(row_updated=credit_res)
 
 
@@ -82,6 +83,7 @@ async def deduct_credit(credit_type, user):
         logger.debug(
             f"in deduct credit >>>{user=},>>>{credit_type=}>>>>>{users.c.profile_credit=}>>>>>{user.profile_credit-1=}"
         )
+        logger.debug(f"{users.c.email_credit=}")
         values = ""
         updated_profile = user.profile_credit - 1
         logger.debug(f"in deduct credit >>>{updated_profile=}")
@@ -105,7 +107,6 @@ async def deduct_credit(credit_type, user):
         logger.debug(f"{values=}")
         update_query = users.update().values(values).where(users.c.id == user.id)
         logger.debug(f"{update_query=}")
-
         if not (row := await database.execute(update_query)):
             logger.debug(f"updated....{row=}")
             logger.warning("Invalid Query Results")
